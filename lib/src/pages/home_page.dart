154a5722a42b6904e3611af5e28f146374a19d1e
christianmocha <letmy.4@hotmail.com>
2021-02-17 19:17:56 -0500

    aplicacion de peliculas con flutter terminado

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:peliculas/src/providers/peliculas_provider.dart';
+import 'package:peliculas/src/search/search_delegate.dart';
 
 import 'package:peliculas/src/widgets/card_swiper_widget.dart';
 import 'package:peliculas/src/widgets/movie_horizontal.dart';
-
 
 class HomePage extends StatelessWidget {
 
@@ -14,41 +14,47 @@ class HomePage extends StatelessWidget {
 
     peliculasProvider.getPopulares();
 
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
-        title: Text('Peliculas en cines'),
+        title: Text('Películas en cines'),
         backgroundColor: Colors.indigoAccent,
         actions: <Widget>[
           IconButton(
-            icon: Icon( Icons.search),
-            onPressed: (){},
+            icon: Icon( Icons.search ),
+            onPressed: () {
+              showSearch(
+                context: context, 
+                delegate: DataSearch(),
+                // query: 'Hola'
+                );
+            },
           )
         ],
       ),
       body: Container(
         child: Column(
-          mainAxisAlignment:  MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             _swiperTarjetas(),
             _footer(context)
           ],
         ),
       )
-      
+       
     );
   }
 
   Widget _swiperTarjetas() {
 
-
     return FutureBuilder(
       future: peliculasProvider.getEnCines(),
-
       builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-        if ( snapshot.hasData){
-        return  CardSwiper(peliculas: snapshot.data,); 
-        }else{
+        
+        if ( snapshot.hasData ) {
+          return CardSwiper( peliculas: snapshot.data );
+        } else {
           return Container(
             height: 400.0,
             child: Center(
@@ -56,41 +62,51 @@ class HomePage extends StatelessWidget {
             )
           );
         }
+        
       },
     );
 
+
+
+    
+
+
   }
+
 
   Widget _footer(BuildContext context){
 
     return Container(
       width: double.infinity,
       child: Column(
-        crossAxisAlignment:  CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
             padding: EdgeInsets.only(left: 20.0),
-            child: Text('Populares', style: Theme.of(context).textTheme.subhead)
+            child: Text('Populares', style: Theme.of(context).textTheme.subhead  )
           ),
-          SizedBox(height: 5.0,),
-
+          SizedBox(height: 5.0),
 
           StreamBuilder(
-            stream:peliculasProvider.popularesStream, 
+            stream: peliculasProvider.popularesStream,
             builder: (BuildContext context, AsyncSnapshot<List> snapshot) {
-
-              if ( snapshot.hasData ){
-                return MovieHorizontal(peliculas: snapshot.data, siguientePagina:  peliculasProvider.getPopulares,);
-              }else{
-
+              
+              if ( snapshot.hasData ) {
+                return MovieHorizontal( 
+                  peliculas: snapshot.data,
+                  siguientePagina: peliculasProvider.getPopulares,
+                );
+              } else {
                 return Center(child: CircularProgressIndicator());
               }
             },
           ),
+
         ],
       ),
     );
-  }
 
+
+  }
 
 }
